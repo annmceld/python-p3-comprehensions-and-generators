e23e70129c9f4acb3f621dc6cecc069831499bc1
Ann McElderry <thayerann1@gmail.com>
2023-05-02 11:05:04 -0500

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even_num = [i for i in num_list if i % 2 == 0]
+    return even_num
     pass
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_sentence = [i + '!' for i in sentence_list]
+    return exclamation_sentence
